b5d9afa756cc5d02e372366e879453cb9405a77d
Chuanhui <knuthocean@163.com>
2012-05-13 17:39:02 +0800

    add sorter2.h

--- sorter2.h
+++ sorter2.h
@@ -1,12 +1,30 @@
 
+/*
+template <typename T>
+inline bool my_less(T& first, T& second)
+{
+    return first < second;
+}
+*/
+
+template <typename T>
+class Comparator
+{
+public:
+    inline bool operator() (T& first, T& second) const
+    {
+        return first < second;
+    }
+};
+
 template <typename T> class sorter_t
 {
 public:
     // 快速排序
-    int qsort(T* begin, T* end);
+    int qsort(T* begin, T* end, Comparator<T>& my_less);
 
 private:
-    const static int sthreshold = 1;
+    const static int sthreshold = 16;
 
 private:
     // 插入排序 (假定序列的最小元素在[begin, begin + threshold)之间)
@@ -16,7 +34,7 @@ private:
     inline void _linear_insert(T* first, T* last);
 
     // 划分元素使得序列被分成两部分，且左边元素 <= pivot <= 右边元素
-    inline T* _partition(T* begin, T* end, T pivot);
+    inline T* _partition(T* begin, T* end, T pivot, Comparator<T>& my_less);
 
     // median_of_three: 交换元素使得first < middle < last
     inline const T& _median_of_three(T& first, T& middle, T& last)
@@ -44,7 +62,7 @@ private:
     }
 
     // Qsort 排序算法
-    void _qsort_loop(T* begin, T* end);
+    void _qsort_loop(T* begin, T* end, Comparator<T>& my_less);
 };
 
 template <typename T> void sorter_t<T>::_insertion_sort(T* begin, T* end, int threshold)
@@ -92,20 +110,23 @@ template <typename T> void sorter_t<T>::_linear_insert(T* first, T* last)
     }
 }
 
-template <typename T> T* sorter_t<T>::_partition(T* begin, T* end, T pivot)
+template <typename T> T* sorter_t<T>::_partition(T* begin, T* end, T pivot, Comparator<T>& my_less)
 {
     while (true)
     {
-        while (*begin < pivot)
+        //while (*begin < pivot)
+	while (my_less(*begin, pivot))
         {
             ++begin;
         }
         --end;
-        while (pivot < *end)
+        //while (pivot < *end)
+	while (my_less(pivot, *end))
         {
             --end;
         }
-        if (!(begin < end))
+        //if (!(begin < end))
+	while (!my_less(begin, end))
         {
             break;
         }
@@ -115,7 +136,7 @@ template <typename T> T* sorter_t<T>::_partition(T* begin, T* end, T pivot)
     return begin;
 }
 
-template <typename T> int sorter_t<T>::qsort(T* begin, T* end)
+template <typename T> int sorter_t<T>::qsort(T* begin, T* end, Comparator<T>& my_less)
 {
     int ret = 0;
     if (NULL == begin || NULL == end)
@@ -125,25 +146,25 @@ template <typename T> int sorter_t<T>::qsort(T* begin, T* end)
     }
     else
     {
-        _qsort_loop(begin, end);
+        _qsort_loop(begin, end, my_less);
         _insertion_sort(begin, end, sorter_t<T>::sthreshold);
     }
     return ret;
 }
 
-template <typename T> void sorter_t<T>::_qsort_loop(T* begin, T* end)
+template <typename T> void sorter_t<T>::_qsort_loop(T* begin, T* end, Comparator<T>& my_less)
 {
     while (end - begin > sorter_t<T>::sthreshold)
     {
         T median_of_three = _median_of_three(*begin, *(begin + (end - begin)/2), *(end - 1));
-        T* cut = _partition(begin, end, median_of_three);
+        T* cut = _partition(begin, end, median_of_three, my_less);
         if (cut - begin >= end - cut)
         {
-            _qsort_loop(cut, end);
+            _qsort_loop(cut, end, my_less);
             end = cut;
         } else
         {
-            _qsort_loop(begin, cut);
+            _qsort_loop(begin, cut, my_less);
             begin = cut;
         }
     }
